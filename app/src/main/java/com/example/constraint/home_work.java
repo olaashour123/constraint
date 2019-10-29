cb5d1b337859c1e492ceee9e8c815917f2fa5d9a
olaashour <ola.artist97@gmail.com>
2019-10-29 02:58:29 +0200

    Initial Commit

---
+++ app/src/main/java/com/example/constraint/home_work.java
@@ -0,0 +1,14 @@
+package com.example.constraint;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class home_work extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_work);
+    }
+}
